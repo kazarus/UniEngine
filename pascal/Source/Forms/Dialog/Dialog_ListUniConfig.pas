9cca3899e2d1a0afd8b411fd950ed172f89fb451
kazarus <kazarus@qq.com>
2016-03-25 14:08:48 +0800

    20160325

--- pascal/Source/Forms/Dialog/Dialog_ListUniConfig.pas
+++ pascal/Source/Forms/Dialog/Dialog_ListUniConfig.pas
@@ -4,10 +4,13 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, System.Rtti,
+  FMX.Layouts, FMX.Grid, FMX.Controls.Presentation, FMX.StdCtrls;
 
 type
   TDialogListUniConfig = class(TForm)
+    Panl_1: TPanel;
+    StringGrid1: TStringGrid;
   private
   public
   end;
