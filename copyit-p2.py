5d2378961f3e99b1322e367fd11b60591e12e747
kazarus <kazarus@qq.com>
2016-11-14 00:50:39 +0800

    20161114

--- copyit-p2.py
+++ copyit-p2.py
@@ -14,9 +14,14 @@ def copyFiles(sourceDir, targetDir):
     global copyFileCounts  
     print sourceDir  
     print u"%s 当前处理文件夹%s已处理%s 个文件" %(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())), sourceDir,copyFileCounts)  
-    for f in os.listdir(sourceDir):  
+    for f in os.listdir(sourceDir):
+        
+        if f ==".git":
+            continue
+        
         sourceF = os.path.join(sourceDir, f)  
-        targetF = os.path.join(targetDir, f)  
+        targetF = os.path.join(targetDir, f)
+        print sourceF
                  
         if os.path.isfile(sourceF):  
             #创建目录  
